df1a00796654589a1c08192b881924cc93ece872
noah <noah@gmail.com>
2026-04-21 17:27:49 +0800

    fix

--- packages/env/src/utils/dynamicScriptCtx.ts
+++ packages/env/src/utils/dynamicScriptCtx.ts
@@ -1,28 +1,14 @@
-import { mchcLogger } from "../logger";
-import { gen_rt_ctx, IRTCtx } from "./runtime_ctx";
+import { safe_evaluate_context } from "@lm_fe/utils";
+import { gen_rt_ctx } from "./runtime_ctx";
 
 
 
 
-export function dynamicScriptExecute(cb: (ctx: IRTCtx) => void, props?: any): boolean {
-    try {
-        cb(gen_rt_ctx('instance_ctx', props))
-        return false
-    } catch (error: any) {
-        mchcLogger.error('dynamicScriptExecute', { error })
-        return true
-    }
 
-}
 
 export function getSymbolFromDynamicScript<T = any>(str: any, props?: any, default_v?: T) {
-    let ret: T | undefined
-    if (typeof str !== 'string') return ret
-    const is_err = dynamicScriptExecute((ctx) => {
-        eval(str)
-    }, props)
-    if (is_err) {
-        ret = default_v
-    }
-    return ret
+    const { is_err, data } = safe_evaluate_context(str, () => gen_rt_ctx('instance_ctx', props))
+    if (is_err) return default_v
+    return data
 }
+

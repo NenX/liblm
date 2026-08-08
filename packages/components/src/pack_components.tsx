32dfe06270e49d372f93fd77b8c44c270a2ec4fc
XIONGZHIQIANG25\xzq <953761972@qq.com>
2026-08-08 10:39:39 +0800

    医生端电子签名、诊断、医嘱导入优化

--- packages/components/src/pack_components.tsx
+++ packages/components/src/pack_components.tsx
@@ -3,14 +3,17 @@ import { MyAddress } from './MyAddress'
 import { MyImageEditor } from './MyImageEditor'
 import { PatientSelect } from './PatientSelect'
 import { ArraySingle } from './ArraySingle'
+import { SignImageUpload } from './SignImageUpload'
 
 export * from './MyAddress'
 export * from './MyImageEditor'
 export * from './PatientSelect'
+export * from './SignImageUpload'
 export const pack_components = {
     ...packs,
     MyAddress,
     MyImageEditor,
     ArraySingle,
-    PatientSelect
+    PatientSelect,
+    SignImageUpload
 }
